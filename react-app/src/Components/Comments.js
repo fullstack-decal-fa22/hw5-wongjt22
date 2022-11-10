20db980b63916ce813dd33273208b0dbdc5e6698
Jackson Wong <wongjt@berkeley.edu>
2022-11-10 13:50:27 -0800

    IDK what im doing

---
+++ react-app/src/Components/Comments.js
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import axios from "axios";
+
+const Comments = ({ postId, comments: initialComments }) => {
+  const [comments, setComments] = useState(initialComments);
+  const [newComment, setNewComment] = useState('');
+
+  const handleSubmitComment = () => {
+    console.log(newComment)
+    // Un-comment the lines below to complete your solution
+    // ====================
+    axios.post('http://localhost:3002/posts', { newComment }).then((res) => {
+      setNewComment(newComment);
+      setComments(comments.append(newComment))
+    })
+  }
+
+  return (
+    <div style={{ border: '1px solid black'}}>
+      {comments && comments.map((comment, i) => (
+        <div key={i + comment}>
+        <p>
+          {comment}
+        </p>
+        </div>
+      ))}
+      <div>
+        <input value={newComment} onChange={e => setNewComment(e.target.value)}/>
+      </div>
+      <button  onClick={handleSubmitComment}>
+        Submit
+      </button>
+    </div>
+  )
+}
+
+export default Comments;
---
+++ react-app/src/Components/Feed.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Post from "./Post";
+import NewPost from "./NewPost";
+
+const Feed = () => {
+  // Un-comment the lines below to complete your solution
+  // ====================
+
+  const [data, setData] = useState();
+
+  const getPostsData = () => {
+    axios
+      .get('http://localhost:3002/posts') //THIS IS YOUR URL OF YOUR API
+      .then((data) => setData(data.data)) //PROMISE API, THAT MEANS WHEN YOU GET THE DATA WHAT DO I DO WITH IT
+      .catch((error) => console.log(error));  //ERROR CATCHING IN CASE WE RECIEVE AN ERROR
+  };
+
+  useEffect(() => {
+    getPostsData();
+  }, [])
+
+  // ====================
+
+  return (
+    <div style={{ maxWidth: '600px', marginLeft: 'auto', marginRight: 'auto'}}>
+      {
+        data && data.map(d =>
+          <Post title={d.title} body={d.body} comments={d.comments} id={d.id} key={d.id} />
+        )
+      }
+
+      <NewPost func={getPostsData} />
+    </div>
+  )
+
+}
+
+
+export default Feed;
